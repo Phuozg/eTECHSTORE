9cb53b4b4de2412afb62569acc35d3ae00a2154b
Huycoconut <107424687+Huycoconut@users.noreply.github.com>
2024-04-29 12:33:42 +0700

    add image dependencies

---
+++ app_mobile/etechstore/lib/utlis/constants/image_key.dart
@@ -0,0 +1,11 @@
+class ImageKey {
+  //Image
+  static const String logoEtechStore = 'assets/images/logo.png';
+  static const String loading = 'assets/images/loading.png';
+
+  //Icons
+  static const String iconGoogle = 'assets/icons/icon_google.png';
+  static const String successAnimation = 'assets/icons/success_animation.json';
+  static const String loadingAnimation = 'assets/icons/loading_animation.json';
+  static const String smsPhoneAnimation = 'assets/icons/sms_phone_animation.json';
+}
